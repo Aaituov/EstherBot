dd92e51634b1f3a815d6cbe798a18d5178ae71ee
Aaituov <aaituov@gmail.com>
2016-07-13 16:30:05 +0600

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('OOpppaaaa So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Привет! Рад пообщаться. Напишите HELLO !.')
                 .then(() => 'speak');
         }
     },
